745bf9fc270df375389bdd78eb67683bad243126
AryaGupta01 <aryagupta101@gmail.com>
2025-02-01 21:12:36 +0530

    short-url

--- 11_Url_Shortner/index.js
+++ 11_Url_Shortner/index.js
@@ -4,11 +4,16 @@ const mongoose = require('mongoose');
 const {connectToMongodb}= require('./connect')
 const urlRouter = require('./routes/url');
 const Url= require('./models/user')
+const path = require('path');
 
 connectToMongodb('mongodb://127.0.0.1:27017/11_Url_shortner').then(()=> console.log("MongoDB connected"));
 
 // middleware
 app.use(express.json());
+
+// view engine
+app.set('view engine','ejs');
+app.set('views',path.resolve('./views'));
 
 // router
 app.use('/url',urlRouter);  
@@ -32,6 +37,9 @@ app.get('/url/:shortId',async(req,res)=> {
 }
 )
 
+app.get('/test',async (req,res)=>{
+    return res.render('home');
+})
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
